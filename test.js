8aeaf44228983cb9e906cfe17fa3716a1942e9ef
Aayyi <julien.lavocat@gmail.com>
2019-07-15 14:57:54 +0200

    Adding movie search

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 const TMDBClient = require("./index");
 
+require("dotenv").config();
+
 const tmdb = new TMDBClient(process.env.tmdbApiKey, "fr-FR");
+
+test();
+
+async function test() {
+    try {
+        const movie = await tmdb.search().movie("1001 pattes");
+        console.log(movie);
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
